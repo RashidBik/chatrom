6be4373585221e397dd0dcedda605e02ded29c13
rashidbik <abdurashid.bigzad1999@gmail.com>
2022-10-20 23:30:39 +0430

    eliza init

--- src/lib/Button.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let style = 'primary';
-    export let type = 'button';
-    export let disabled = false;
-</script>
-
-<button {type} {disabled} class={style} >
-    <slot></slot>
-</button>
